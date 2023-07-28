92b6c83b5071b5d310ef0c6f2695697ccbbdff2a
Laxmi PANDEY <laxmi.pandey@soprasteria.come>
2023-07-28 11:01:30 +0530

    css update

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -5,41 +5,41 @@ import { Answer, Quiz } from '../category/category.component';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  styleUrls: ['./result.component.scss'],
 })
 export class ResultComponent implements OnInit {
-
-  constructor(private result: QuizResultService) { }
- quizz: Quiz[] = [];
- correctAnsCount : number = 0;
+  constructor(private result: QuizResultService) {}
+  quizDetail: Quiz[] = [];
+  correctAnsCount: number = 0;
   ngOnInit(): void {
-    this.result.result$.subscribe((data:Quiz[])=>{this.quizz = data});
+    this.result.result$.subscribe((data: Quiz[]) => {
+      this.quizDetail = data;
+    });
   }
-  getBtnClass(option: Answer, quiz:Quiz){
-
-    let btnClass: string =''; 
-    if(option?.selected){
-      if(option?.answer === quiz?.correctAnswer){
+  getBtnClass(option: Answer, quiz: Quiz) {
+    let btnClass: string = '';
+    if (option?.selected) {
+      if (option?.answer === quiz?.correctAnswer) {
         btnClass = 'btn-success';
         this.correctAnsCount++;
-      } else{
+      } else {
         btnClass = 'btn-danger';
       }
-    } else if(option?.answer === quiz?.correctAnswer){
+    } else if (option?.answer === quiz?.correctAnswer) {
       btnClass = 'btn-success';
-    } else{
+    } else {
       btnClass = 'btn-outline-success';
     }
-  return btnClass;
+    return btnClass;
   }
 
-  getColorClass(){
-if(this.correctAnsCount<2){
-    return 'red';
-  } else if(this.correctAnsCount>1 && this.correctAnsCount<4){
-    return 'yellow';
-  } else{
-    return 'green';
-  }
+  getColorClass() {
+    if (this.correctAnsCount < 2) {
+      return 'red';
+    } else if (this.correctAnsCount > 1 && this.correctAnsCount < 4) {
+      return 'yellow';
+    } else {
+      return 'green';
+    }
   }
 }
